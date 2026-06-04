3884358febc83c6b6254460d8ffd319de44bef16
jjackson112 <jjackson11211@gmail.com>
2026-06-04 13:19:22 -0400

    create tables - models complete - see tables in Docker

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,25 @@
 import os
 from flask import Flask
+from extensions import db
 from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
-
-
-db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
 
+    # DB Configuration
     app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+    # Init DB - connect
     db.init_app(app)
 
-app = create_app()
+    # Import models before create_all
+    from models.user import User
+    from models.product import Product
+    from models.variant import Variant
+
+    with app.app_context():
+       db.create_all()
+
+    return app
--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -9,53 +9,6 @@ auth_bp = Blueprint("auth", __name__, url_prefix='api/auth')
 
 @auth_bp.route("/login", methods=["POST"])
 def login():
-data = request.get_json() or {}
-    identifier = data.get('identifier', '').strip()
-    password = data.get('password', '').strip()
-
-    user = User.query.filter(
-        or_(
-            User.username == identifier,
-            User.email == identifier
-        )
-    ).first()
-
-    print("identifier", identifier)
-    print("password", password)
-
-    if user:
-        print("stored username", user.username)
-        print("stored email", user.email)
-        print("stored password", user.check_password(password))
-
-    if not user or not user.check_password(password):
-        return jsonify({'error': "Invalid username or password"}), 401
-
-    secret = current_app.config['SECRET_KEY']
-    if not secret:
-        return jsonify({"error": "Server misconfiguration"}), 500 
-
-    # Generate JWT token - payload (data)
-    # access only token
-    token = jwt.encode({
-        'user_id': user.id,
-        'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=2)
-    }, secret, algorithm='HS256') # HS256 to decode and catch expired/invalid tokens
-    
-    refresh_token = jwt.encode({
-        'user_id': user.id,
-        'exp': datetime.datetime.utcnow() + datetime.timedelta(days=5)
-    }, secret, algorithm='HS256')
-
-    if isinstance(token, bytes):
-        token = token.decode('utf-8')
-    
-    return jsonify({
-        'token': token,
-        'username': user.username,
-        'refresh_token' : refresh_token,
-        'user': user.to_dict() # Helpful to send user info back on login
-    }), 200
 
 @auth_bp.route("/register", methods=["POST"])
 def register():
