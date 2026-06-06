86031855bdb202a6523cef7a40e02e5df4d92891
jjackson112 <jjackson11211@gmail.com>
2026-06-06 08:11:48 -0400

    add email to register route - in user model

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,14 +1,36 @@
-from extensions import db
 import jwt
 from jwt import ExpiredSignatureError, InvalidTokenError
-from sqlalchemy import or_
+from extensions import db
 from flask import Blueprint, request, jsonify
 import datetime
-from models.product import Product
-from models.variant import Variant
 from models.user import User
 
 auth_bp = Blueprint("auth", __name__, url_prefix='/api/auth')
+
+@auth_bp.route("/register", methods=["POST"])
+def register():
+    data = request.get_json()
+
+    username = data.get("username", "").strip()
+    password = data.get("password", "").strip()
+    email = data.get("email", "").strip()
+
+    if not username.strip() or not password.strip() or not email.strip():
+        return jsonify({'error': "Username and password are required."}), 400
+    
+    # existing user
+    existing_user = User.query.filter((User.username == username) | (User.email == email)).first() 
+    if existing_user:
+        return jsonify({"Username has already been taken"}), 400
+    
+    # new user
+    new_user = User(username=username, email=email)
+    new_user.set_password(password)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'message': "New user successfully registered."}), 201
 
 @auth_bp.route("/login", methods=["POST"])
 def login():
@@ -22,26 +44,3 @@ def login():
     if not user:
         return jsonify({"Username not found"})
 
-@auth_bp.route("/register", methods=["POST"])
-def register():
-    data = request.get_json()
-
-    username = data.get("username", "").strip()
-    password = data.get("password", "").strip()
-
-    if not username.strip() or not password.strip():
-        return jsonify({'error': "Username and password are required."}), 400
-    
-    # existing user
-    existing_user = User.query.filter(User.username == username).first()
-    if existing_user:
-        return jsonify({"Username has already been taken"}), 400
-    
-    # new user
-    new_user = User(username=username)
-    new_user.set_password(password)
-
-    db.session.add(new_user)
-    db.session.commit()
-
-    return jsonify({'message': "New user successfully registered."}), 201
