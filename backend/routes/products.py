a29ece45dbf46aa9583eac8fe7f60f773b65c256
jjackson112 <jjackson11211@gmail.com>
2026-06-07 13:48:41 -0400

    PATCH + DELETE products

--- backend/routes/products.py
+++ backend/routes/products.py
@@ -3,7 +3,7 @@ from extensions import db
 from models.product import Product
 from services.token import token_required
 
-products_bp = Blueprint("/products", __name__, url_prefix='api/products')
+products_bp = Blueprint("products", __name__, url_prefix='api/products')
 
 @products_bp.route("", methods=["POST"])
 @token_required
@@ -16,7 +16,7 @@ def create_product():
 # get list of products
 @products_bp.route("", methods=["GET"])
 @token_required
-def get_products_list():
+def get_products_list(current_user):
     
     query = Product.query.filter_by(user_id=current_user.id)
 
@@ -25,18 +25,30 @@ def get_products_list():
 # get a single product
 @products_bp.route("/<int:id>", methods=["GET"])
 @token_required
-def get_single_product():
+def get_single_product(current_user, product_id):
 
-    query = Product.query.filter_by(id=products.id,user_id=current_user.id)
+    query = Product.query.filter_by(id=product_id,user_id=current_user.id)
 
     return jsonify({"message": "Single product is here."}), 200
 
 @products_bp.route("/<int:id>", methods=["PATCH"])
 @token_required
-def update_product(id):
-    pass
+def update_product(product_id):
+    # fetch the note, but verify owner
+    product = Product.query.filter_by(id=product_id)
+
+    data = request.get_json()
+
+    if not data:
+        return jsonify({"message": "Product updated"}), 200
 
 @products_bp.route("/<int:id>", methods=["DELETE"])
 @token_required
-def delete_product(id):
-    pass
+def delete_product(current_user, product_id):
+    # if the note exists but belongs to another person, a 404 error appears
+    product = Product.query.filter_by(id=product_id, user_id=current_user.id).first_or_404()
+
+    db.session.delete(product)
+    db.session.commit()
+
+    return jsonify({"message": "Product deleted"}), 204 # successful request but no content required
