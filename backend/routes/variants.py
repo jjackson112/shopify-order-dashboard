a533cbc77ab261784800094af6d2f2df47c41ce9
jjackson112 <jjackson11211@gmail.com>
2026-06-07 17:11:49 -0400

    Product model imported - variant model doesn't have user_id column

--- backend/routes/variants.py
+++ backend/routes/variants.py
@@ -1,28 +1,25 @@
 from flask import Blueprint, request, jsonify
 from extensions import db
+from models.product import Product
 from models.variant import Variant
 from services.token import token_required
 
 variants_bp = Blueprint("variants", __name__, url_prefix='api/variants')
 
-@variants_bp.route("/int:<variant_id>", methods=["GET"])
-@token_required
-def get_variant(variant_id):
-   # give 404 error if not found
-   variant = Variant.query.get_or_404(variant_id)
-
-   return jsonify({"variant": variant.to_dict()}), 200
-
 # single resource endpoint
 @variants_bp.route("/<int:<variant_id>", methods=["GET"])
 @token_required
 def get_single_variant(current_user, variant_id):
-   variant = Variant.query.filter_by(id=variant_id, user_id=current_user.id).first_or_404()
+   variant = Variant.query.join(Product).filter(
+      Variant.id == variant_id
+   )
+   
    return jsonify(variant.to_dict()), 200
 
 @variants_bp.route("/<int:<variant_id>", methods=["PATCH"])
 @token_required
-def update_variants():
+def update_variants(current_user, variant_id):
+   
 
 @variants_bp.route("/<int:<variant_id>", methods=["DELETE"])
 @token_required
