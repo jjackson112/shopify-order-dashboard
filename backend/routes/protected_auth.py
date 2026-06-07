36c08b6ac3440efe067cd3e48c87391dac19a82a
jjackson112 <jjackson11211@gmail.com>
2026-06-07 11:52:01 -0400

    add services/token.py - reusable JWT decorator implementation

--- backend/routes/protected_auth.py
+++ backend/routes/protected_auth.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, jsonify
+from services.token import token_required
 
 # add protected route to test decorator
 protected_auth_bp = Blueprint("protected_auth", __name__, url_prefix='/api/auth')
 
 @protected_auth_bp.route("/protected", methods=["GET"])
 @token_required
-def protected_route(user)
+def protected_route(user):
     return jsonify({"message": f"Hello {user.username}, your token is valid!"})
---
+++ backend/services/token.py
@@ -0,0 +1,52 @@
+import jwt
+from jwt import ExpiredSignatureError, InvalidTokenError
+from flask import request, jsonify, current_app
+from extensions import db
+from functools import wraps
+from models.user import User
+
+# JWT is stateless - not stored in db
+# 3 parts - header (metadata), payload (data), + signature (token integrity)
+# the server only knows the secret key
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs): # args - extra positional arguments, kwargs - extra named arguments
+        token = None
+
+        # Authorization header check tells system it's a JWT
+        if 'Authorization' in request.headers:
+            auth_header = request.headers['Authorization']
+            # handles bearer and just token
+            token = auth_header.split(" ")[1] if " " in auth_header else auth_header
+
+        if not token:
+            return jsonify({'error': 'Token is missing'}), 401
+        
+        try:
+            # Try to get secret from Flask config first, then Environment
+            secret = current_app.config['SECRET_KEY']
+            if not secret:
+                return jsonify({'error': 'Server configuration error (Secret Key missing)'}), 500
+            
+            data = jwt.decode(token, secret, algorithms=['HS256'])
+            
+            # User.query.filter_by(id=data['user_id']).first() could work too
+            current_user = db.session.get(User, data["user_id"]) 
+
+            if not current_user:
+                return jsonify({'error': 'User not found!'}), 404
+            
+        except ExpiredSignatureError:
+            return jsonify({'error': 'Token has expired!'}), 401
+        
+        except InvalidTokenError:
+            return jsonify({'error': 'Invalid token!'}), 401
+        
+        except Exception as e:
+            return jsonify({'error': f'Unexpected error: {str(e)}'}), 500
+
+        # pass the current_user object into the route function
+        return f(current_user, *args, **kwargs)
+    return decorated
+
