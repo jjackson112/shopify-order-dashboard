815e7a067c66979c074b8ab7fe30a975c23df366
jjackson112 <jjackson11211@gmail.com>
2026-06-07 14:06:21 -0400

    add to_dict() + edits

--- backend/routes/products.py
+++ backend/routes/products.py
@@ -18,29 +18,45 @@ def create_product(current_user):
 @token_required
 def get_products_list(current_user):
     
-    query = Product.query.filter_by(user_id=current_user.id)
+    products = Product.query.filter_by(user_id=current_user.id)
 
-    return jsonify({"message": "Products list is here."}), 200
+    return jsonify({
+        "message": "Products list is here.",
+        "products": [product.to_dict() for product in products]
+        }), 200
 
 # get a single product
 @products_bp.route("/<int:id>", methods=["GET"])
 @token_required
 def get_single_product(current_user, product_id):
 
-    query = Product.query.filter_by(id=product_id,user_id=current_user.id)
+    product = Product.query.filter_by(id=product_id,user_id=current_user.id).first_or_404()
 
-    return jsonify({"message": "Single product is here."}), 200
+    return jsonify({
+        "message": "Single product is here.",
+        "product": product.to_dict()
+    }), 200
 
 @products_bp.route("/<int:id>", methods=["PATCH"])
 @token_required
-def update_product(product_id):
+def update_product(current_user, product_id):
     # fetch the note, but verify owner
-    product = Product.query.filter_by(id=product_id)
+    product = Product.query.filter_by(id=product_id, user_id=current_user.id).first_or_404()
 
     data = request.get_json()
+    title = data.get("title")
+    description = data.get("description")
 
-    if not data:
-        return jsonify({"message": "Product updated"}), 200
+    if not title:
+    
+    if not description:
+
+    db.session.commit()
+
+    return jsonify({
+        "message": "Product updated.",
+        "product": product.to_dict()
+    }), 200
 
 @products_bp.route("/<int:id>", methods=["DELETE"])
 @token_required
@@ -51,4 +67,4 @@ def delete_product(current_user, product_id):
     db.session.delete(product)
     db.session.commit()
 
-    return jsonify({"message": "Product deleted"}), 204 # successful request but no content required
+    return "", 204 # successful request but no content required
