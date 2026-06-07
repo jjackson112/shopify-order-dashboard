5390d2f5af09376c4d465133879b980b49be4944
jjackson112 <jjackson11211@gmail.com>
2026-06-07 11:10:43 -0400

    protected route

---
+++ backend/routes/protected_auth.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, jsonify
+
+# add protected route to test decorator
+protected_auth_bp = Blueprint("protected_auth", __name__, url_prefix='/api/auth')
+
+@protected_auth_bp.route("/protected", methods=["GET"])
+@token_required
+def protected_route(user)
+    return jsonify({"message": f"Hello {user.username}, your token is valid!"})
