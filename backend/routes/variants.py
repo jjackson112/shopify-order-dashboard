08da94f6aa7ff27872c0c78a693025c4744093eb
jjackson112 <jjackson11211@gmail.com>
2026-06-07 17:23:15 -0400

    DELETE no longer needs user_id for Variant - use same query as GET + PATCH

--- backend/routes/variants.py
+++ backend/routes/variants.py
@@ -4,10 +4,10 @@ from models.product import Product
 from models.variant import Variant
 from services.token import token_required
 
-variants_bp = Blueprint("variants", __name__, url_prefix='api/variants')
+variants_bp = Blueprint("variants", __name__, url_prefix='/api/variants')
 
 # single resource endpoint
-@variants_bp.route("/<int:<variant_id>", methods=["GET"])
+@variants_bp.route("/int:<variant_id>", methods=["GET"])
 @token_required
 def get_single_variant(current_user, variant_id):
    variant = Variant.query.join(Product).filter(
@@ -17,7 +17,7 @@ def get_single_variant(current_user, variant_id):
    
    return jsonify({"variant": variant.to_dict()}), 200
 
-@variants_bp.route("/<int:<variant_id>", methods=["PATCH"])
+@variants_bp.route("/int:<variant_id>", methods=["PATCH"])
 @token_required
 def update_variants(current_user, variant_id):
     variant = Variant.query.join(Product).filter(
@@ -52,10 +52,13 @@ def update_variants(current_user, variant_id):
     }), 200
 
 
-@variants_bp.route("/<int:<variant_id>", methods=["DELETE"])
+@variants_bp.route("/<int:variant_id>", methods=["DELETE"])
 @token_required
 def delete_variants(current_user, variant_id):
-   variant = Variant.query.filter_by(id=variant_id, user_id=current_user.id).first_or_404()
+   variant = Variant.query.join(Product).filter(
+       Variant.id == variant_id,
+       Product.user_id == current_user.id
+   ).first_or_404()
 
    db.session.delete(variant)
    db.session.commit()
