8abd238ae733357dc0c1d814ff50fe23a90e2069
jjackson112 <jjackson11211@gmail.com>
2026-06-07 23:19:31 -0400

    separate state in RegisterForm

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -4,32 +4,33 @@ import { Form, FormLayout, TextField, Button } from "@shopify/polaris";
 // autocomplete (boolean) gives the browser the ability to autocomplete input elements
 
 function RegisterForm() {
-    const [identifier, setIdentifier] = useState("")
+    const [username, setUsername] = useState("")
+    const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
 
  return (
    <Form>
      <TextField 
         label="Username" 
-        value={identifier}
-        onChange={setIdentifier}
+        value={username}
+        onChange={setUsername}
         autoComplete="username"
-    />
-
-     <TextField 
-        label="Password" 
-        type="password" 
-        value={password}
-        onChange={setPassword}
-        autoComplete="password"
     />
 
      <TextField 
         label="email" 
         type="email" 
-        value={identifier}
-        onChange={setIdentifier}
+        value={email}
+        onChange={setEmail}
         autoComplete="email"
+    />
+
+    <TextField 
+        label="Password" 
+        type="password" 
+        value={password}
+        onChange={setPassword}
+        autoComplete="password"
     />
 
     <Button submit variant="primary">Register</Button>
