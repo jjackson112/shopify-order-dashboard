9eed4868b6e58ae3cbb9d0434d529a12a59b5cb8
jjackson112 <jjackson11211@gmail.com>
2026-06-08 10:20:37 -0400

    fetch products + navigate

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -3,7 +3,32 @@ import { api } from "../api";
 import { useNavigate } from "react-router-dom";
 
 function Dashboard() {
+    const [products, setProducts] = useState([])
 
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const res = await api.get("/products")
+                console.log(res)
+
+                // data into state
+                setProducts(res.products)
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        fetchProducts()
+    }, [])
+
+    const handleView = (id) => {
+        navigate(`/products/${id}`)
+    }
+
+    return (
+        
+    )
 }
 
 export default Dashboard;
