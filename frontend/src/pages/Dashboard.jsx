50ae9f3d360c55f72193dbb04416f6513a81e8d5
jjackson112 <jjackson11211@gmail.com>
2026-06-08 10:15:51 -0400

    start Dashboard

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+import { useEffect, useState } from "react";
+import { api } from "../api";
+import { useNavigate } from "react-router-dom";
+
+function Dashboard() {
+
+}
+
+export default Dashboard;
