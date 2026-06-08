a2af701045bffc1be347aa026970edf849197fa3
jjackson112 <jjackson11211@gmail.com>
2026-06-07 21:53:00 -0400

    Login page

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react;"
+import { FormLayout, TextField } from "@shopify/polaris";
+
+// autocomplete (boolean) gives the browser the ability to autocomplete input elements
+
+function LoginForm() {
+    const [identifier, setIdentifier] = useState("")
+    const [password, setPassword] = useState("")
+
+ return (
+   <Form>
+     <TextField 
+        label="Username/Email" 
+        value={identifier}
+        onChange={setIdentifier}
+        autoComplete="username"
+    />
+
+     <TextField 
+        label="Password" 
+        type="password" 
+        value={password}
+        onChange={setPassword}
+        autoComplete="password"
+    />
+
+    <Button submit variant="primary">Login</Button>
+   </Form>
+ );
+}
+
+export default LoginForm;
