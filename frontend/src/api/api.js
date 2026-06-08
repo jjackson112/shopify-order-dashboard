165dfcb613681cc9b69e02dab910f091136e2db9
jjackson112 <jjackson11211@gmail.com>
2026-06-08 09:01:43 -0400

    create a central API layer

---
+++ frontend/src/api/api.js
@@ -0,0 +1,6 @@
+// central API layer - call requests from here
+// looks separately into localStorage
+// inject token from context - make API depend on token
+// exporting a static object > move API into a factory function
+
+const BASE_URL = "http://localhost:5000/api";
