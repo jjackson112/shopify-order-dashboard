af8bdfd245275505e2635181a60d3319c8f354e5
jjackson112 <jjackson11211@gmail.com>
2026-06-07 20:56:24 -0400

    install Shopify Polaris + import to main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import '@shopify/polaris/build/esm/styles.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
