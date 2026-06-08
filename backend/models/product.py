8e7344073d124f4ae65b63cf4d5c747b56039346
jjackson112 <jjackson11211@gmail.com>
2026-06-08 13:52:31 -0400

    drop tables - fix Product model + rebuild

--- backend/models/product.py
+++ backend/models/product.py
@@ -5,7 +5,7 @@ class Product(db.Model):
     __tablename__ = "products"
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     title = db.Column(db.String(75), nullable=False)
     description = db.Column(db.String(1000), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
