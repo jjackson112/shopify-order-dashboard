0b02027b5fd9010bc35af1da48af24d370ec3a0f
jjackson112 <jjackson11211@gmail.com>
2026-06-07 22:17:19 -0400

    Add public routes - login + register - to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,23 @@
 import { AppProvider } from '@shopify/polaris'
-import Dashboard from "./pages/Test"
+import { Route, Routes } from "react-router-dom";
+import AuthLayout from "./components/AuthLayout";
+import Dashboard from "./pages/Test";
+import LoginForm from './pages/Login';
+import RegisterForm from './pages/Register';
 
 function App() {
   return (
     <AppProvider>
+      <Routes>
+        {/* Public Routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
+        
+        {/* Protected Routes */}
+
+      </Routes>
       <Test />
       </AppProvider>
   )
