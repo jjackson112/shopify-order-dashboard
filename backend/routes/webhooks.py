380bba09ff940c851c7bba20532b120cd47a193b
jjackson112 <jjackson11211@gmail.com>
2026-07-09 16:30:21 -0400

    flexible webhook route to avoid separating Flask routes for each event

--- backend/routes/webhooks.py
+++ backend/routes/webhooks.py
@@ -2,10 +2,20 @@ from flask import Blueprint, jsonify, request
 
 webhooks_bp = Blueprint("webhooks", __name__, url_prefix="/api/webhooks")
 
-@webhooks_bp.route("/orders/create", methods=["POST"])
-def create_order():
+@webhooks_bp.route("/webhooks/<topic>", methods=["POST"])
+def get_webhook():
     data = request.get_json() or {}
 
-    print("Order created", data)
+    if not data:
+        return ({"error": "Invalid JSON"}), 400
+    
+    print("Webhook event", data)
 
     return jsonify({"received": True}), 200
+
+
+# @webhooks_bp.route("/orders/create", methods=["POST"])
+# def create_order():
+#    data = request.get_json() or {}
+#    print("Order created", data)
+#    return jsonify({"received": True}), 200
