433f9962055daa2956f3e4c3b4028c025502672f
jjackson112 <jjackson11211@gmail.com>
2026-07-09 15:29:58 -0400

    fix routes/webhooks.py with / and get 200 status code in ThunderClient

--- backend/routes/webhooks.py
+++ backend/routes/webhooks.py
@@ -1,11 +1,11 @@
 from flask import Blueprint, jsonify, request
 
-webhooks_bp = Blueprint("webhooks", __name__, url_prefix="api/webhooks")
+webhooks_bp = Blueprint("webhooks", __name__, url_prefix="/api/webhooks")
 
 @webhooks_bp.route("/orders/create", methods=["POST"])
 def create_order():
     data = request.get_json() or {}
 
     print("Order created", data)
-    
+
     return jsonify({"received": True}), 200
