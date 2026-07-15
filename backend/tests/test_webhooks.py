f6a67faa4b9d88ba55d0ef1f517f65acf6fc211a
jjackson112 <jjackson11211@gmail.com>
2026-07-15 18:02:50 -0400

    test valid webhook with pytest

--- backend/tests/test_webhooks.py
+++ backend/tests/test_webhooks.py
@@ -4,3 +4,15 @@
 
 # No Thunder Client ↓ POST request ↓ localhost ↓ receive response
 # Pytest does it all for me
+
+def test_valid_webhook(client):
+    # simulate the request
+    response = client.post(
+        "/api/webhooks/orders/create"
+    )
+    
+    # check the status code
+    assert response.status_code == 200
+    
+    # check JSON
+    assert response.json["error"] == "Invalid signature"
