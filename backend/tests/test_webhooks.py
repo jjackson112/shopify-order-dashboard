00d4d6d22758bd1cfa5f3e581265a8ae7d098a25
jjackson112 <jjackson11211@gmail.com>
2026-07-14 22:23:56 -0400

    pytest notes

---
+++ backend/tests/test_webhooks.py
@@ -0,0 +1,6 @@
+# test is signature is valid and then invalid
+# is the header missing?
+# was the JSON rejected at all?
+
+# No Thunder Client ↓ POST request ↓ localhost ↓ receive response
+# Pytest does it all for me
