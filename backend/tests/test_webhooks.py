fd146c694bdf33b5e31da27ada7d046eccaaaf01
jjackson112 <jjackson11211@gmail.com>
2026-07-17 08:16:24 -0400

    helper function to help test with fake input

--- backend/tests/test_webhooks.py
+++ backend/tests/test_webhooks.py
@@ -5,23 +5,43 @@
 # No Thunder Client ↓ POST request ↓ localhost ↓ receive response
 # Pytest does it all for me
 
+# missing input - fake payload, signature + headers - create helper function
+
+def create_valid_webhook():
+    payload = b'{"id": 123}' # b is raw bytes
+    secret = "test_secret"
+    
+
 def test_valid_webhook(client):
+    payload, signature, headers = (
+        create_valid_webhook()
+    )
+
     # simulate the request
     response = client.post(
-        "/api/webhooks/orders/create"
+        "/api/webhooks/orders/create",
+        data=payload,
+        headers=headers
     )
 
     # check the status code
     assert response.status_code == 200
 
     # check JSON
-    assert response.json["error"] == "Invalid signature"
+    assert response.json["received"] is True
 
-def test_invalid_webhook(client):
+def test_invalid_json(client):
     response = client.post(
-        "api/webhooks/orders/create"
+        "/api/webhooks/orders/create"
     )
 
     assert response.status_code == 400
-
     assert response.json["error"] == "Invalid JSON"
+
+def test_missing_signature(client):
+    response = client.post(
+        "/api/webhooks/orders/create"
+    )
+
+    assert response.status_code == 401
+    assert response.json["error"] == "Invalid signature"
