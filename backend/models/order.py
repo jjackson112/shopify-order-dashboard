06223d8842264d32a19a4ad25b40450cf7e8ca17
jjackson112 <jjackson11211@gmail.com>
2026-07-18 23:03:19 -0400

    refine

--- backend/models/order.py
+++ backend/models/order.py
@@ -4,13 +4,16 @@ from datetime import UTC, datetime
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     
-    order_number = db.Column(db.Integer)
-    order_status = db.Column(db.String)
-    total_price = db.Column(db.Integer)
+    order_number = db.Column(db.Integer, nullable=False, unique=True)
+    order_status = db.Column(db.String(50), nullable=False)
+
+    # db.Numeric(10, 2) is for money - allow for 10 digits + 2 of those digits are after the decimal point
+    total_price = db.Column(db.Numeric(10, 2), nullable=False)
 
     created_at = db.Column(db.DateTime(timezone=True), nullable=False, default=lambda: datetime.now(UTC))
 
     items = db.relationship(
         "OrderItem",
-        back_populates="order"
+        back_populates="order",
+        cascade="all, delete-orphan" # if the parent dies, so do the children (OrderItems)
     )
--- backend/models/order_item.py
+++ backend/models/order_item.py
@@ -2,12 +2,14 @@ from extensions import db
 from datetime import UTC, datetime
 
 class OrderItem(db.Model):
-    order_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, db.ForeignKey("order.id"), nullable=False)
     
-    product_name = db.Column(db.String)
+    product_name = db.Column(db.String(255), nullable=False)
     quantity = db.Column(db.Integer)
-    total_price = db.Column(db.Integer)
-    sku = db.Column(db.String, nullable=False)
+    unit_price = db.Column(db.Numeric(10, 2), nullable=False)
+    total_price = db.Column(db.Numeric(10, 2), nullable=False)
+    sku = db.Column(db.String(100), nullable=True)
 
     created_at = db.Column(db.DateTime(timezone=True), nullable=False, default=lambda: datetime.now(UTC))
 
