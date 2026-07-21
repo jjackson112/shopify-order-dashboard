40c2b28f0f02e1f4edbf1bb24c39c0f51dc67238
jjackson112 <jjackson11211@gmail.com>
2026-07-20 22:14:35 -0400

    notes about test order

--- backend/tests/create_order.py
+++ backend/tests/create_order.py
@@ -3,7 +3,7 @@ from models.order_item import OrderItem
 from models.webhook_event import WebhookEvent
 from tests.create_test_signature import create_test_signature
 
-def test_order(client):
+def test_order(client, app):
     payload = b'''
     {
         "order_number": 1001,
@@ -41,3 +41,28 @@ def test_order(client):
 
     # check JSON
     assert response.json["received"] is True
+
+    with app.app_context(): # db "wakes up"
+
+        # find my order
+        order = Order.query.filter_by(order_number=1001).first()
+
+        # does the order exist?
+        assert order is not None
+        assert order.order_status == "paid"
+        assert str(order.total_price) == "59.98"
+
+        assert len(order.items) == 1
+
+        item = order.items[0]
+
+        assert item.product_name == "Black T-Shirt"
+        assert item.quantity == 2
+        assert str(item.unit_price) == "29.99"
+        assert str(item.total_price) == "59.98"
+
+        event = WebhookEvent.query.filter_by(topic="orders/create").order_by(WebhookEvent.id.desc()).first()
+
+        # did the webhook event save?
+        assert event is not None
+        assert event.processed is True
