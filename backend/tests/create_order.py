6cbbe43a3105f9544c4e952f5158ae3d56b2d28c
jjackson112 <jjackson11211@gmail.com>
2026-07-20 21:47:04 -0400

    create test order with columns from Order model

--- backend/tests/create_order.py
+++ backend/tests/create_order.py
@@ -3,3 +3,11 @@ from models.order_item import OrderItem
 from models.webhook_event import WebhookEvent
 from tests.create_test_signature import create_test_signature
 
+def test_order():
+    payload = b'''
+    {
+        "order_number": 1001,
+        "order_status": "paid",
+        "total_price": "59.98"
+    }
+    '''
