0ab9b8aca1e2322cc94a46f328f8b083392cf371
jjackson112 <jjackson11211@gmail.com>
2026-07-30 19:38:01 -0400

    add fallback to shipping address

--- frontend/src/components/OrderDetail.jsx
+++ frontend/src/components/OrderDetail.jsx
@@ -116,13 +116,19 @@ function OrderDetail() {
                                     Items: {order.line_items?.length || 0}
                                 </Text>
 
-                                <Text as="p">
-                                    {order.shipping_address.address1}, {order.shipping_address.address2}
-                                    {order.shipping_address.city}, {order.shipping_address.province}{" "}
-                                    {order.shipping_address.zip}
-                                </Text>
+                                {order.shipping_address ? (
+                                    <BlockStack gap="100">
+                                        <Text as="p">
+                                            {order.shipping_address.address1}, {order.shipping_address.address2}
+                                            {order.shipping_address.city}, {order.shipping_address.province}{" "}
+                                            {order.shipping_address.zip}
+                                        </Text>
 
-                                <Text>{order.shipping_address.country}</Text>
+                                        <Text>{order.shipping_address.country}</Text>
+                                    </BlockStack>
+                                ) : (
+                                    <Text as="p" tone="subdued">No shipping address is available</Text>
+                                )}
                             </BlockStack>
                         </Card>
                     </div>
