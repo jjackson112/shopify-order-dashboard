00301874814cbd22482de573ad7acddc28fd2d6b
jjackson112 <jjackson11211@gmail.com>
2026-07-31 17:37:01 -0400

    CustomerDetail created - useEffect

---
+++ frontend/src/components/CustomerDetail.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import { api } from "../api";
+import "../App.css";
+import { customerName } from "../utils/customer_name";
+
+function CustomerDetail() {
+    const [showCustomer, setShowCustomer] = useState([])
+
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState("")
+
+    useEffect(() => {
+        const fetchCustomerDetail = async () => {
+            try {
+                setLoading(true)
+                setError("")
+
+                const data = await api.get("/order/shopify")
+                console.log(data)
+
+                setShowCustomer(data.showCustomer || [])
+
+            } catch (err) {
+                console.log(error)
+                setError(err.message || "Customer details unavailable")
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        fetchCustomerDetail()
+        
+    }, [])
+
+
+    return ()
+}
+
+export default CustomerDetail;
