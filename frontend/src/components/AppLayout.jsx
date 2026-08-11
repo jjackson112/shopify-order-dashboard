30abec2687c403ac855f5c881365a5850a1cb3c3
jjackson112 <jjackson11211@gmail.com>
2026-08-11 15:00:39 -0400

    create Footer component + add to AppLayout

--- frontend/src/components/AppLayout.jsx
+++ frontend/src/components/AppLayout.jsx
@@ -1,6 +1,7 @@
 import "../App.css";
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 function AppLayout() {
   return (
@@ -10,6 +11,8 @@ function AppLayout() {
       <main>
         <Outlet />
       </main>    
+
+      <Footer />
       
     </div>
   )
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer>
+            <h4>© Kona Coffee & Co | 2026 </h4>
+        </footer>
+    )
+}
+
+export default Footer;
