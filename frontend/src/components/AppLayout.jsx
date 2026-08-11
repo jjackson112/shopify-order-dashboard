b5683514d489e4ed9db19e86a847eccb2f116343
jjackson112 <jjackson11211@gmail.com>
2026-08-11 13:38:22 -0400

    import Header to AppLayout not AuthLayout

--- frontend/src/components/AppLayout.jsx
+++ frontend/src/components/AppLayout.jsx
@@ -1,9 +1,15 @@
 import "../App.css";
+import { Header } from "../components/Header";
 
 function AppLayout() {
   return (
     <div className="app-shell">
-      <Outlet />
+      <Header />
+
+      <main>
+        <Outlet />
+      </main>    
+      
     </div>
   )
 }
--- frontend/src/components/AuthLayout.jsx
+++ frontend/src/components/AuthLayout.jsx
@@ -1,15 +1,10 @@
 import "../App.css";
 import { Outlet } from "react-router-dom";
-import { Header } from "../components/Header";
 
 function AuthLayout({ children }) {
     return (
         <div className="auth-shell">
-            <Header />
-
-            <main>
-                <Outlet />
-            </main>
+            <Outlet />
         </div>
     )
 }
