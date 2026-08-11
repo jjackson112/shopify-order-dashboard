50d8f49ddd6db9d8a8e27b1f621bbd0da72539c8
jjackson112 <jjackson11211@gmail.com>
2026-08-11 13:52:48 -0400

    import Header correctly + Outlet to AppLayout

--- frontend/src/components/AppLayout.jsx
+++ frontend/src/components/AppLayout.jsx
@@ -1,5 +1,6 @@
 import "../App.css";
-import { Header } from "../components/Header";
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
 
 function AppLayout() {
   return (
